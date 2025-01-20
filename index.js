cb130a683367e4f368c825cbaf627d94db4a9b59
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-01-20 17:01:01 +0600

    REST API GET POST PUT

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 // create an express server
-const express = require('express');
-const app = express();
-const port = 3000;
-// create a route for the app
-app.get('/', (req, res) => {
-    res.send('Hello World');
-    }
-);
-// another route for json response
-app.get('/json', (req, res) => {
-    res.json({ title:'hello', id: 1, message: 'Hello World'});
-    }
-);
-// anothsendFile
-
-app.get('/html', (req, res) => {
-    res.sendFile(__dirname + '/main.html');
-    });
-// make the server listen to requests
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-    }
-);
+    const express = require('express');
+    const app = express();    
+    const port = 5001;
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    const mytodo= {
+        id: 1,
+        title: "My first todo",
+        completed: false
+    };
+    // another route for json response
+        app.get("/", (req, res) => {
+            res.json(mytodo);
+        });
+    
+    // put route
+        app.put("/", (req, res) => {
+            const body=req.body;
+            newtodo=body.title;
+            mytodo.title=newtodo;
+            console.log(newtodo);
+            res.json(mytodo);
+        }
+    );
+        app.get('/html', (req, res) => {
+            res.sendFile(__dirname + '/main.html');
+            });
+        // make the server listen to requests
+        app.listen(port);
+            
