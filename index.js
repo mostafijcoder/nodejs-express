278ea08f811e6b22355211c81f04534bd9f5a26f
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-01-20 07:15:42 +0600

    spi.http testing

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
     res.send('Hello World');
     }
 );
+// another route for json response
+app.get('/json', (req, res) => {
+    res.json({ title:'hello', id: 1, message: 'Hello World'});
+    }
+);
+// anothsendFile
+
+app.get('/html', (req, res) => {
+    res.sendFile(__dirname + '/main.html');
+    });
 // make the server listen to requests
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
