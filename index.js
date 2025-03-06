64cfd0ebc9816ffc93d69bb74245c019922dc8e0
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-06 10:38:47 +0600

    first nodejs

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
     const express = require('express');
     const app = express();    
     const port = 5001;
+    var serveStatic = require('serve-static');
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    const mytodo= {
+    app.use(serveStatic(__dirname + '/public'))
+            
+    /*const mytodo= [{
         id: 1,
         title: "My first todo",
         completed: false
-    };
+    }];
     // another route for json response
         app.get("/", (req, res) => {
             res.json(mytodo);
@@ -28,4 +31,5 @@
             });
         // make the server listen to requests
         app.listen(port);
-            
+        */
+    
