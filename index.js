9d6b097416c0979d0ef9de46e18fd304b2d18c5f
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-12 00:24:20 +0600

    own file web server creation which will serve base html at root and respose 404 in other route

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var http = require('http');
-var server = http.createServer(function (request, response) {
+var fs=require('fs');
+
+function send404Response(response){
+    response.writeHead(404,{"Content-Type":"text/plain"});
+    response.write("Error 404: Page not found!");
+    response.end();
+}
+
+var server = http.createServer(function (req, res) {
    console.log('request starting...');
-   // respond
-   response.write('hello client!');
-   response.end();
+   if (req.method === 'GET' && req.url === '/'){
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         fs.createReadStream('./index.html').pipe(res);
+    }
+    else{
+        send404Response(res);
+    }
+
 });
 server.listen(3005);
 console.log('Server running at http://127.0.0.1:3005/');
